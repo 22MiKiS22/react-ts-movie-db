2894219ed8ffb0e2b842c27de8461806a85132d5
22MiKiS22 <michalskor@gmail.com>
2021-07-11 17:20:21 +0200

    warnings deleted

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -13,7 +13,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
   //@ts-ignore
-  const [_user, setUser] = useContext(Context);
+  const [_user, setUser] = useContext(Context); // eslint-disable-line
   const navigate = useNavigate();
 
   const handleSubmit = async () => {
